5b2ed371aa107b57ec8b6e788976c493d43e1138
M3str3 <namestre3@protonmail.com>
2024-05-08 19:38:09 +0200

    Update field.rs: New string update impl

--- src/field.rs
+++ src/field.rs
@@ -9,7 +9,7 @@ impl Field<u32> {
     /// Updates the buffer at the specified offset with the new value for u32.
     pub fn update(&mut self, buffer: &mut Vec<u8>, new_value: u32) {
         self.value = new_value;
-        let mut bytes = [0u8; 4]; 
+        let mut bytes: [u8; 4] = [0u8; 4]; 
         bytes[0] = (new_value & 0xff) as u8;
         bytes[1] = ((new_value >> 8) & 0xff) as u8;
         bytes[2] = ((new_value >> 16) & 0xff) as u8;
@@ -22,9 +22,23 @@ impl Field<u16> {
     /// Updates the buffer at the specified offset with the new value for u16.
     pub fn update(&mut self, buffer: &mut Vec<u8>, new_value: u16) {
         self.value = new_value;
-        let mut bytes = [0u8; 2];  
+        let mut bytes: [u8; 2] = [0u8; 2];  
         bytes[0] = (new_value & 0xff) as u8;
         bytes[1] = ((new_value >> 8) & 0xff) as u8;
         buffer[self.offset..self.offset + self.size].copy_from_slice(&bytes[..self.size]);
+    }
+}
+
+impl Field<String> {
+    /// Updates the buffer at the specified offset with the new UTF-8 encoded string.
+    pub fn update(&mut self, buffer: &mut Vec<u8>, new_value: String) {
+        self.value = new_value;
+        let bytes: &[u8] = self.value.as_bytes(); 
+        if bytes.len() > self.size {
+            panic!("New string value exceeds the allocated field size.");
+        }
+
+        buffer[self.offset..self.offset + bytes.len()].copy_from_slice(bytes);
+
     }
 }
