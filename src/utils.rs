38efcb5b1d0f2acdcc94f9eb74988acb803b63b3
M3str3 <namestre3@protonmail.com>
2024-09-14 21:38:05 +0200

    Refactor utils: simplify extract functions with map for byte conversion

--- src/utils.rs
+++ src/utils.rs
@@ -4,8 +4,7 @@ pub fn extract_u64(buffer: &[u8], offset: usize) -> Result<u64, FileParseError> 
     buffer
         .get(offset..offset + 8)
         .ok_or(FileParseError::BufferOverflow)
-        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow))
-        .and_then(|bytes| Ok(u64::from_le_bytes(bytes)))
+        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow)).map(u64::from_le_bytes)
 }
 
 pub fn extract_u32(buffer: &[u8], offset: usize) -> Result<u32, FileParseError> {
@@ -15,8 +14,7 @@ pub fn extract_u32(buffer: &[u8], offset: usize) -> Result<u32, FileParseError> 
         .and_then(|bytes| {
             bytes
                 .try_into()
-                .map_err(|_| FileParseError::BufferOverflow)
-                .and_then(|bytes| Ok(u32::from_le_bytes(bytes)))
+                .map_err(|_| FileParseError::BufferOverflow).map(u32::from_le_bytes)
         })
 }
 
@@ -24,6 +22,5 @@ pub fn extract_u16(buffer: &[u8], offset: usize) -> Result<u16, FileParseError> 
     buffer
         .get(offset..offset + 2)
         .ok_or(FileParseError::BufferOverflow)
-        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow))
-        .and_then(|bytes| Ok(u16::from_le_bytes(bytes)))
+        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow)).map(u16::from_le_bytes)
 }
