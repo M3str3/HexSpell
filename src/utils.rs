96d6e1a0bb172761c67b63f19d9ee9773c09652d
M3str3 <namestre3@protonmail.com>
2024-09-14 21:48:49 +0200

    format utils: Formatting with `cargo fmt`

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,8 @@ pub fn extract_u64(buffer: &[u8], offset: usize) -> Result<u64, FileParseError> 
     buffer
         .get(offset..offset + 8)
         .ok_or(FileParseError::BufferOverflow)
-        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow)).map(u64::from_le_bytes)
+        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow))
+        .map(u64::from_le_bytes)
 }
 
 pub fn extract_u32(buffer: &[u8], offset: usize) -> Result<u32, FileParseError> {
@@ -14,7 +15,8 @@ pub fn extract_u32(buffer: &[u8], offset: usize) -> Result<u32, FileParseError> 
         .and_then(|bytes| {
             bytes
                 .try_into()
-                .map_err(|_| FileParseError::BufferOverflow).map(u32::from_le_bytes)
+                .map_err(|_| FileParseError::BufferOverflow)
+                .map(u32::from_le_bytes)
         })
 }
 
@@ -22,5 +24,6 @@ pub fn extract_u16(buffer: &[u8], offset: usize) -> Result<u16, FileParseError> 
     buffer
         .get(offset..offset + 2)
         .ok_or(FileParseError::BufferOverflow)
-        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow)).map(u16::from_le_bytes)
+        .and_then(|bytes| bytes.try_into().map_err(|_| FileParseError::BufferOverflow))
+        .map(u16::from_le_bytes)
 }
